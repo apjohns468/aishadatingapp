c21f000dd4d65372f8b3da511712aa3cd1b4f280
apjohns468 <87904474+apjohns468@users.noreply.github.com>
2021-07-24 11:24:01 -0500

    Pick random question

--- src/components/DatingQuestionnair.vue
+++ src/components/DatingQuestionnair.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import {ref} from 'vue'
 import { useI18n } from 'vue-i18n'
 const { t, availableLocales, locale } = useI18n()
 const toggleLocales = () => {
@@ -7,8 +8,26 @@ const toggleLocales = () => {
   locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
 }
 
+let questionIndex = null
+
+const questions = [
+  //0
+  'What’s your favorite international food?',
+  'Are you a morning person or a night owl?',
+  'What’s your favorite restaurant?',
+  'How many siblings do you have?',
+  'What would be your dream job?',
+  'What would you do if had enough money to not need a job?',
+  'Who is your favorite author?',
+  'What was the last show you binge-watched?',
+  'What TV series do you keep coming back to and re-watching'
+]
+
 const getANewQuestion = () => {
-  alert ("Button Click")
+  const randomNumber = Math.floor(Math.random() * questions.length)
+  
+  questionIndex = randomNumber
+  //'return questions[randomNumber]
 }
 
 </script>
@@ -17,6 +36,7 @@ const getANewQuestion = () => {
 
     
   <div> 
+    {{ questions[randomNumber] }}
     <button 
       class="QuestionButton"
       title="Randomize a new question" 

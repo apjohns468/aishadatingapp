070ceb121d759f1e56214e44824bdb6f6167671c
apjohns468 <87904474+apjohns468@users.noreply.github.com>
2021-07-24 10:22:06 -0500

    Recognize button click

--- src/components/DatingQuestionnair.vue
+++ src/components/DatingQuestionnair.vue
@@ -1,20 +1,28 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-import { isDark, toggleDark } from '~/logic'
 const { t, availableLocales, locale } = useI18n()
 const toggleLocales = () => {
   // change to some real logic
   const locales = availableLocales
   locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
 }
+
+const getANewQuestion = () => {
+  alert ("Button Click")
+}
+
 </script>
 
 <template>
 
     
   <div> 
-    <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark">
-      New Question
+    <button 
+      class="icon-btn mx-2 !outline-none" 
+      title="Randomize a new question" 
+      @click="getANewQuestion"
+    >
+          New Question
     </button>
   </div>
 
